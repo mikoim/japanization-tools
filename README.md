7b7b513a2dc3af072efe955bf1887a269ab46d4c
Eshin Kunishima <ek@esh.ink>
2016-07-23 01:40:23 +0900

    キュレーターから情報を集めるスクリプトを追加

--- README.md
+++ README.md
@@ -10,4 +10,7 @@ pip3.5 install -r requirements.txt
 # Generate BB Code files based on "日本語化情報DB".
 # These files will be outputted at "output" directory.
 python3.5 generate_files.py
+
+# Gather recommended games on Steam Group
+python3.5 gather_curation.py | tee result.txt
 ```
---
+++ gather_curation.py
@@ -0,0 +1,39 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+import re
+import sys
+
+regex_app_id = re.compile('app/([0-9]+)/')
+
+start = 0
+
+while True:
+    result = requests.get('http://steamcommunity.com/groups/japanization/ajaxgetrecommendations/render/', params={
+        'query': '',
+        'start': start
+    })
+
+    print(result.url, file=sys.stderr)
+
+    obj = json.loads(result.text)
+
+    if not obj['success']:
+        print(obj, file=sys.stderr)
+        exit()
+
+    results_html = BeautifulSoup(obj['results_html'], 'lxml')
+
+    for product in results_html.find_all(class_='curation_app_block'):
+        app_id = regex_app_id.search(product.find(class_='curation_app_block_content').a['href']).group(1)
+        app_name = product.find(class_='curation_app_block_name').string.strip()
+        summary = product.find(class_='curation_app_block_blurb').get_text().strip()
+
+        try:
+            link = product.find(class_='highlighted_recommendation_link').a['href']
+        except:
+            link = ''
+
+        print('\t'.join([app_id, app_name, summary, link]))
+
+    start += 5
