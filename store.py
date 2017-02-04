32659aacd6b9e3314e4d4cf61a3997a46d50d5b7
Eshin Kunishima <ek@esh.ink>
2017-02-05 03:13:14 +0900

    add Steam API wrapper

---
+++ store.py
@@ -0,0 +1,48 @@
+import json
+import sys
+
+import requests
+
+
+class SteamException(Exception):
+    pass
+
+
+class Store(object):
+    l = None
+    cc = None
+
+    def __init__(self, language='en', country='jp'):
+        self.l = language
+        self.cc = country
+
+    def appdetails(self, app_id: int) -> dict:
+        url = 'http://store.steampowered.com/api/appdetails'
+        cache_file_path = 'cache/{:d}.json'.format(app_id)
+        api_str = str(app_id)
+
+        try:
+            with open(cache_file_path) as cached:
+                return json.load(cached)[api_str]['data']
+        except IOError:
+            print('app_id = {:d} is not cached'.format(app_id), file=sys.stderr)
+
+        r = requests.get(url=url, params={'l': self.l, 'cc': self.cc, 'appids': app_id})
+
+        if r.status_code != 200:
+            raise SteamException('Steam API returned non-200 status')
+
+        j = r.json()
+
+        if api_str not in j or not j[api_str]['success']:
+            raise SteamException('Steam API returned unexpected error')
+
+        with open(cache_file_path, mode='w') as cached:
+            cached.write(r.text)
+
+        return j[api_str]['data']
+
+    @staticmethod
+    def support_japanese(data: dict) -> bool:
+        k = 'supported_languages'
+        return k in data and 'Japanese' in data[k]
