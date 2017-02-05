76b78365dfd4ca93e6f9b2531c7bc220d57e8127
Eshin Kunishima <ek@esh.ink>
2017-02-05 23:15:10 +0900

    initial vue client

---
+++ www/steam-jp-finder/src/components/Hello.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="hello">
+    <md-whiteframe>
+      <md-toolbar md-theme="blue">
+        <span class="md-title">My app dashboard</span>
+      </md-toolbar>
+    </md-whiteframe>
+
+    <md-layout md-gutter="24" md-align="center">
+      <md-layout md-flex="35">
+        <md-card md-with-hover>
+          <md-card-header>
+            <div class="md-title">Title goes here</div>
+            <div class="md-subhead">Subtitle here</div>
+          </md-card-header>
+
+          <md-card-content>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed
+            accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+          </md-card-content>
+
+          <md-card-actions>
+            <md-button>Action</md-button>
+            <md-button>Action</md-button>
+          </md-card-actions>
+        </md-card>
+      </md-layout>
+
+      <md-layout md-flex="35">
+        <md-card md-with-hover>
+          <md-card-header>
+            <div class="md-title">Title goes here</div>
+            <div class="md-subhead">Subtitle here</div>
+          </md-card-header>
+
+          <md-card-content>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed
+            accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+          </md-card-content>
+
+          <md-card-actions>
+            <md-button>Action</md-button>
+            <md-button>Action</md-button>
+          </md-card-actions>
+        </md-card>
+      </md-layout>
+    </md-layout>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'hello',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .md-card {
+    margin-top: 10px;
+  }
+</style>
